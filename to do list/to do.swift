fedd3df2ced1a310e0ec003a24c0d58a889f899f
Jaritzi Lopez <jnlopez2005@gmail.com>
2022-06-27 16:47:43 -0500

    iteration 0 complete

---
+++ to do list/to do.swift
@@ -0,0 +1,14 @@
+//
+//  to do.swift
+//  to do list
+//
+//  Created by Scholar on 6/27/22.
+//
+
+import UIKit
+
+class To_Do{
+    var name =  "THIS IS BAD DON'T WANT THIS"
+    var important = false
+}
+
