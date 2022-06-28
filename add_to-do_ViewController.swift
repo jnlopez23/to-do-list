a22199d8532ec153f73720d50a1c5a290c489c56
Jaritzi Lopez <jnlopez2005@gmail.com>
2022-06-28 13:17:55 -0500

    iteration 1 complete

---
+++ add_to-do_ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  add_to-do_ViewController.swift
+//  to do list
+//
+//  Created by Scholar on 6/28/22.
+//
+
+import UIKit
+
+class add_to_do_ViewController: UIViewController {
+
+    var previousVC = to_do_list_TableViewController()
+    
+    @IBOutlet weak var titleTextField: UITextField!
+     @IBOutlet weak var importantSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+        let toDo = To_Do()
+
+        if let titleText = titleTextField.text {
+            toDo.name = titleText
+            toDo.important = importantSwitch.isOn
+          }
+          previousVC.to_dos.append(toDo)
+          previousVC.tableView.reloadData()
+    
+      }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
